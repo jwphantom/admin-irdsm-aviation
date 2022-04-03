291519791d3358dec0d8f9a5faf0d7d4996456da
jwphantom <44881349+jwphantom@users.noreply.github.com>
2022-04-03 17:28:38 +0100

    first commit

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(__dirname + '/dist/irdsm-aviation'));
+app.use(express.static(__dirname + '/dist/admin-irdsm-aviation'));
 
 app.get('/*', (req,res,next) => {
-    res.sendFile(path.join(__dirname + '/dist/irdsm-aviation/index.html'));
+    res.sendFile(path.join(__dirname + '/dist/admin-irdsm-aviation/index.html'));
 });
 
 
