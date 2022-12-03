858114a130a6a88a67066ad45e7c063068124e9b
jwphantom <44881349+jwphantom@users.noreply.github.com>
2022-12-03 14:42:21 +0100

    set configuration server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const path = require("path");
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + "/dist/admin-irdsm-aviation"));
+app.use(express.static(__dirname + "/dist/adminirdsmaviation"));
 
 app.get("/*", function (req, res) {
     res.sendFile(path.join(__dirname + "/dist/adminirdsmaviation/index.html"));
