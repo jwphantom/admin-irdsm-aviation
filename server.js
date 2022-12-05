2e32726f245788d1c3e4a3444427031b82e7c2dc
jwphantom <44881349+jwphantom@users.noreply.github.com>
2022-12-05 09:17:09 +0100

    change dirname on server.js

--- server.js
+++ server.js
@@ -5,10 +5,12 @@ const path = require("path");
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + "/dist/adminirdsmaviation"));
+app.use(express.static(__dirname + "/dist/admin-irdsm-aviation"));
 
 app.get("/*", function (req, res) {
-    res.sendFile(path.join(__dirname + "/dist/adminirdsmaviation/index.html"));
+    res.sendFile(
+        path.join(__dirname + "/dist/admin-irdsm-aviation/index.html")
+    );
 });
 
 // Start the app by listening on the default Heroku port
